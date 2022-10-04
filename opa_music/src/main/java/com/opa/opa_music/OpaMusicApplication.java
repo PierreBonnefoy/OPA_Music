c595536d4e628e86505cb67eb4f9f0aba80c31b3
theflokky <aloyslanapro@gmail.com>
2022-10-04 17:34:17 +0200

    [FEAT] Initial Commit

---
+++ opa_music/src/main/java/com/opa/opa_music/OpaMusicApplication.java
@@ -0,0 +1,13 @@
+package com.opa.opa_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpaMusicApplication.class, args);
+	}
+
+}
---
+++ opa_music/src/test/java/com/opa/opa_music/OpaMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opa.opa_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpaMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
