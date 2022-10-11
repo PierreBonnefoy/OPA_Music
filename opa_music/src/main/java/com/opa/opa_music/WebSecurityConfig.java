4a9d3b71e3ad4768b9948c14f6a37ed980c87750
PierreBonnefoy <pierreeta@hotmail.fr>
2022-10-11 19:29:25 +0200

    [FEAT] Can save, see and delete your favorites

--- opa_music/src/main/java/com/opa/opa_music/WebSecurityConfig.java
+++ opa_music/src/main/java/com/opa/opa_music/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                                 // All Permissions : all pages wich are not in here are going to be
                                 // innaccessible without connexion
                                 .antMatchers("/", "/home", "/register", "/addUser", "/login", "/h2-console**",
-                                                "/css/**", "/js/**", "/images/**", "/search", "/loginRedirect", "/addFav/{link}")
+                                                "/css/**", "/js/**", "/images/**", "/search", "/loginRedirect", "/addFav/{link}&{mail}","/fav/{mail}","/fav","/delFav/{link}&{mail}")
                                 .permitAll()
 
                                 // Default page after a successfull connection
